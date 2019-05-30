320b25501b955a872e110d497d574360a4794315
Guowei Liu A <18042971250@189.cn>
2019-05-30 17:38:47 +0800

    更改时间地区

--- mysite_polls/polls/models.py
+++ mysite_polls/polls/models.py
@@ -1,12 +1,20 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
-from django.db import models
+import datetime
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    # ...
+    def __str__(self):
+        return self.question_text
+
+    def was_published_reently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -14,14 +22,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-
-class Question(models.Model):
-    # ...
-    def __str__(self):
-        return self.question_text
-
-
-class Choice(models.Model):
     # ...
     def __str__(self):
         return self.choice_text
