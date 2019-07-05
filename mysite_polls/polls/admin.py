3eacc924df39b021eafcaa13fcfc761900bacd74
Guowei Liu A <18042971250@189.cn>
2019-07-05 09:29:55 +0800

    自定义后台表单调整顺序

--- mysite_polls/polls/admin.py
+++ mysite_polls/polls/admin.py
@@ -4,5 +4,11 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
 admin.site.register(Choice)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
